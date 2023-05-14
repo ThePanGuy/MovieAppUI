fb8f0af4db5589a2083dbaf9aff898f567624220
pnikolidakis <pnikolidakis@procureship.com>
2023-05-14 21:56:24 +0300

    add a Movies component

--- src/components/Movies.tsx
+++ src/components/Movies.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
+import {usePaging} from "../hooks/PagingHook";
 
 const Movies:React.FunctionComponent = () => {
-
-
+    const paging = usePaging('/movie/page', 12, 0);
 
     return (
         <div>
 
         </div>
-    );
+    )
 };
 
 export default Movies;
