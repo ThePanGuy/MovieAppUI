d2bad44ef658d85c5708fdfa6b7775ad441ffeab
pnikolidakis <pnikolidakis@procureship.com>
2023-11-26 03:34:26 +0200

    login everywhere

--- src/utilities/fetch.ts
+++ src/utilities/fetch.ts
@@ -80,12 +80,12 @@ function parseResponse(response: Response): Promise<CustomResponse> {
         response.json()
             .then((json: any) => {
                 if (response.status === 403) {
-                    document.location.href = '/error-no-access'
+                    document.location.href = '/login'
                 } else if (response.status === 404) {
                     document.location.href = '/error-not-found'
                 } else if (response.status === 401) {
                     if (!(window.location.href.indexOf("login") > -1)) {
-                        document.location.href = '/'
+                        document.location.href = '/login'
                     }
                 } else if (response.status === 426) {
                     resolve({
